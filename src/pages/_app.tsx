f9539bd8b712061a6fa312f5bb28944c67b188a7
Dz0526 <this@dz99.me>
2022-10-11 16:27:10 +0900

    Feat: setup chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
-
 export default MyApp
+
