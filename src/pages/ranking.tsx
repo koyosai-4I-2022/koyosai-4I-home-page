3ecb42fd074b664b50a9c6e429baddd6a7201a76
tnkryoga <tanaryo-miyabi818@outlook.jp>
2022-10-20 17:10:49 +0900

    fix:interaption

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -3,11 +3,14 @@ import { Input, InputGroup,InputRightElement} from '@chakra-ui/react'
 import {SearchIcon} from '@chakra-ui/icons'
 import { Ranking } from 'type/ranking';
 import useSWR from 'swr';
+import React, { useState, useRef } from 'react'
 
 import { fetcher } from 'fetcher';
 
 const RankingPage = () => {
   const { data, error } = useSWR<Ranking[]>('ranking', fetcher);
+  const [inputName, setInputName] = useState('')
+  const refs = useRef(new Array<HTMLDivElement>(data && data.length as number))
   if (error) return 'error';
   if (!data) return 'Loading...';
   return (
@@ -15,16 +18,18 @@ const RankingPage = () => {
       <InputGroup size='lg' width='95%' bg='white' ml='2.5%' mt='2.5%' borderRadius='50%'>
         <Input
           placeholder='名前を入力してください'
+          value={inputName}
+          onChange={(e)=>{setInputName(e.target.value)}}
         />
         <InputRightElement width='4.5rem' >
-          <Button h='1.75rem' bg='white' size='lg'  onClick={()=>{return}}>
+          <Button h='1.75rem' bg='white' size='lg'  onClick={()=>{console.log(inputName)}}>
             <SearchIcon color='#6370EB'/>
           </Button>
         </InputRightElement>
       </InputGroup>
-      {data.map(userData => {
+      {data.map((userData, index) => {
         return (
-          <Flex key={userData.id} alignItems='center'>
+          <Flex key={userData.id} alignItems='center' ref={refs.current[index]}>
             <Circle bg='#6370EB' size='50px' color='white' m='2%'>
               <Text as='b' fontSize={'3xl'}>
                 {userData.rank}
