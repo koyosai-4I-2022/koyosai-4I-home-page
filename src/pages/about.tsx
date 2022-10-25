2346e7ab1362da38948ec1b6583f8a9b5680e38b
lemonadern <taishi.develop@gmail.com>
2022-10-25 23:36:41 +0900

    add fadein animation to AboutPage

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,27 @@
-import { Container, Heading, Stack, Text } from '@chakra-ui/react';
+import { Container, Heading, keyframes, Stack, Text } from '@chakra-ui/react';
 import { CharacterModel } from 'components/CharacterModel';
 import { Layout } from 'components/layouts/Layout';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 const AboutPage = () => {
   return (
     <Layout>
-      <Container mt={3}>
+      <Container mt={3}
+      css={{
+        animationName: `${fadeIn}`,
+        animationDuration: '1s',
+        animationFillMode: 'forwards',
+      }}>
         <Heading as='h1' size='2xl' color='#42B2DF'>
           About
         </Heading>
